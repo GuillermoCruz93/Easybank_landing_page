66319c19b999125d83d8ba1a8d30817782cf1d28
GuillermoCruz93 <g_cp1993@outlook.com>
2021-03-28 23:49:51 -0600

    agrega Hamburger

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,13 @@
-console.log('Hello World');
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+
+btnHamburger.addEventListener('click', function(){
+
+    if(header.classList.contains('open')){ //close Hamburger Menu
+        header.classList.remove('open');
+    }
+
+    else{//Open Hamburger Menu
+        header.classList.add('open');
+    }
+});
